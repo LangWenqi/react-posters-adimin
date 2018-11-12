237f4f77b04e8eff67fe205133e017967a4f3ec2
langwenqi <497379037@qq.com>
2018-11-12 16:47:19 +0800

    hide api host

--- src/api/domains.js
+++ src/api/domains.js
@@ -3,9 +3,8 @@ let domainsFuc = (env) => {
     if (env==='development') {
         domain = 'http://192.168.1.160:8082/crm'
     }else{
-        domain = 'http://manage.i31.com:8082/crm'
+        domain = 'http://192.168.1.160:8082/crm'
     }
-    domain = 'http://192.168.1.160:8082/crm'
     return {
         domain:domain
     }
